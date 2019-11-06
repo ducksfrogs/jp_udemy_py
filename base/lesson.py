f4a19949e8d53980ea61f6bf6999959d821f51ed
ma <ma_yamaki@yahoo.com>
2019-11-06 15:14:47 +0900

    first commit

---
+++ base/lesson.py
@@ -0,0 +1,6 @@
+is_ok = ''
+
+if is_ok:
+    print('ok!')
+else:
+    print('No')
---
+++ base/range.py
@@ -0,0 +1,12 @@
+#for i in range(20):
+#    print(i)
+
+#for i in range(2, 20):
+#    print(i)
+
+
+for i in range(2, 20, 3):
+    print(i)
+
+for _ in range(2, 20):
+    print('Hi!')
